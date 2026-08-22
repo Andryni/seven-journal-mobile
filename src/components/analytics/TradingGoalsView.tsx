9268d0ded65c15120295b06eeca304a3be51815d
Andryni <andry@seven.com>
2026-08-22 22:03:01 +0300

    fix(TradingGoalsView): add missing ActivityIndicator import from react-native

--- src/components/analytics/TradingGoalsView.tsx
+++ src/components/analytics/TradingGoalsView.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ActivityIndicator } from 'react-native';
 import Animated, { FadeIn } from 'react-native-reanimated';
 import { useHaptic } from '../../hooks/useHaptic';
 import { useTheme } from '../../theme';
